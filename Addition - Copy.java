b6ff77d2b4b1a4df5cc9c0e48e177244cd82bcef
molletibobby6309 <bobbymolleti881@gmail.com>
2024-07-11 17:36:36 +0530

    welv

---
+++ Addition - Copy.java
@@ -0,0 +1,9 @@
+public class Addition2{
+public static void main(String args[])
+{
+int num1=500;
+int num2=22;
+int add=num1+num2;
+System.out.println("the addition is :" +add);
+}
+}
---
+++ multiple - Copy.java
@@ -0,0 +1,9 @@
+public class multiple{
+public static void main(String args[])
+{
+int num1=10;
+int num2=20;
+int multi=num1*num2;
+System.out.println("the addition is :" +multi);
+}
+}
